3ef9980b6e2f723addabea7f2a156230b97c196e
Josh Gottsegen <josh@onelightstudios.com>
2026-09-17 15:30:35 -0700

    Refresh Peace Lion imagery from corrected print artwork

--- components/PeaceSpotlight.tsx
+++ components/PeaceSpotlight.tsx
@@ -7,7 +7,7 @@ import { track } from '@vercel/analytics';
 const PeaceSpotlight: React.FC = () => (
   <aside className="peace-spotlight" id="peace-feature" aria-labelledby="peace-spotlight-title">
     <Link to="https://www.troplanduniverse.com/peace" className="peace-spotlight-image" aria-label="View the Peace Lion Tee" onClick={() => track('peace_artwork_shirt_image')}>
-      <img src="/images/peace/mockup-black.jpg" alt="Black Peace Lion Tee with the original gold lion artwork" width={720} height={960} loading="lazy" />
+      <img src="/images/peace/mockup-black-v2-20260917.jpg" alt="Black Peace Lion Tee with the original gold lion artwork" width={720} height={960} loading="lazy" />
     </Link>
     <div className="peace-spotlight-copy">
       <p className="peace-spotlight-eyebrow">STRONG ENOUGH TO CHOOSE PEACE</p>
